f5d3e2984cefc9aeffd4d5e0dd5570e3c35b4aa2
Collin Ahn <collinahn@hufs.ac.kr>
2021-08-03 21:37:11 +0900

    [Modify] simplify cpu temperature logic

--- temp.sh
+++ temp.sh
@@ -2,8 +2,7 @@
 
 TEMP_CPU_RAW=`cat /sys/class/thermal/thermal_zone0/temp`
 TEMP_CPU_INT=$((${TEMP_CPU_RAW}/1000))
-TEMP_CPU_TEMP=$((${TEMP_CPU_RAW}/100))
-TEMP_CPU_DECIMAL=$((${TEMP_CPU_TEMP} % ${TEMP_CPU_INT}))
+TEMP_CPU_DECIMAL=$(($((${TEMP_CPU_RAW}%1000))/100))
 
 TEMP_GPU=`/opt/vc/bin/vcgencmd measure_temp`
 TEMP_GPU=${TEMP_GPU//temp=/}
